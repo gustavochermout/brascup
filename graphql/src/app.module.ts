805b2176a6730ccd27fe641cc2b6aaa34b5cc083
Gustavo Chermout <gustavo_chermout@hotmail.com>
2020-06-28 17:45:22 -0300

    Commit inicial graphql

---
+++ graphql/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
